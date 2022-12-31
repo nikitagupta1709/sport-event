fcdef1ba14af1591ae210d14808d136b8e750930
Nikita Gupta <nikitagupta1709@gmail.com>
2022-12-31 17:09:48 +0530

    get all and get event

--- server/controllers/event.controller.js
+++ server/controllers/event.controller.js
@@ -35,9 +35,107 @@ export const createEvent = async(req, res) =>{
     }
 }
 
-// Join an event
+// get all events
+export const getAllEvents = async (req, res) => {
+    try {
+        let events = await eventModel.find();
+        // console.log(events, "events");
+        res.send({
+            error:false,
+            events: events
+        })
+    } catch (error) {
+        res.status(500).send({
+            message: error
+        });
+    }
+}
 
+// get event 
+export const getEvent = async (req, res) => {
+    try {
+        let eventId =  req.params;
+        console.log(eventId, "eventId");
+        let event = await eventModel.findOne({_id: eventId._id})
+        res.send({
+            error: false,
+            event:event
+        })
+    } catch (error) {
+        res.status(500).send({
+            message: error
+        });
+    }
+}
+
+// requested Join an event
 export const joinedEvent  = async (req,res) => {
+    try {
+        let eventId = req.params;
+        let userId = req.body; 
+
+        let event = await eventModel.findById(eventId._id);
+        let user = await userModel.findById(userId.userId);
+
+        event.players.push({
+            userId : user._id,
+            name:user.name,
+            email:user.email,
+            value:"requested"
+        })
+        user.events.push({
+            eventId : event._id,
+            title:event.title,
+            desc: event.desc,
+            startTime: event.startTime,
+            endTime: event.endTime,
+            address:event.address,
+            lcoation:event.location,
+            players_limit: event.players_limit,
+            picture: event.picture,
+            category: event.category,
+            organizer: event.userId,
+            value:"requested"
+        })
+
+        await eventModel.findByIdAndUpdate(eventId._id,{
+            players:event.players
+        })
+        await userModel.findByIdAndUpdate(userId.userId,{
+            events:user.events
+        })
+        res.send({
+            error:false,
+            message:"You have requested to join this event!"
+        })
+    } catch (error) {
+        res.send({
+            error: error
+        })
+    }
+}
+
+// accepted Join an event
+export const acceptedEvent  = async (req,res) => {
+    try {
+        let eventId = req.params;
+        let userId = req.body; 
+
+        let event = await eventModel.findById({id: eventId._id});
+        let user = await userModel.findById({id: userId.userId});
+
+        console.log(event, "event");
+        console.log(user, "user");
+
+    } catch (error) {
+       return res.send({
+            error: error
+        })
+    }
+}
+
+// rejected
+export const rejectedEvent  = async (req,res) => {
     try {
         let eventId = req.params;
         let userId = req.body; 
--- server/routes/events.route.js
+++ server/routes/events.route.js
@@ -1,14 +1,18 @@
 import express from 'express';
-import { createEvent, joinedEvent } from '../controllers/event.controller.js';
+import { createEvent, getAllEvents, getEvent, rejectedEvent, acceptedEvent, joinedEvent } from '../controllers/event.controller.js';
 
 const eventRouter = express.Router();
 
 eventRouter.post('/create', createEvent);
 
-eventRouter.get('/');
+eventRouter.get('/', getAllEvents);
 
-eventRouter.get('/:_id');
+eventRouter.get('/:_id', getEvent);
 
 eventRouter.post("/requested/:_id", joinedEvent)
+
+eventRouter.post("/accepted/:_id", acceptedEvent)
+
+eventRouter.post("/rejected/:_id", rejectedEvent)
 
 export default eventRouter;
